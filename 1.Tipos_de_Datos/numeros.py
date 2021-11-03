501e1e609b9a81552c06716cba310aaf297557e6
rodrigopsimoes <rodrigo.simoes18@gmail.com>
2021-11-03 09:19:28 +0100

    first commit

---
+++ 1.Tipos_de_Datos/numeros.py
@@ -0,0 +1,2 @@
+
+print("Esta  {0} {1} {2} de {3}".format("es", "una", "cadena", "TEXTO"))
---
+++ unidade 1/ejemplo.py
@@ -0,0 +1,8 @@
+from typing import OrderedDict
+
+
+def decir_hola():
+    print("Hola")
+    print("como")
+    print("esta?")
+decir_hola()
